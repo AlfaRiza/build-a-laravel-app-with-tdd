0fc025028ceeffab25ae5b327c824338f52c9ed8
AlfaRiza <malfariza45@gmail.com>
2020-06-02 01:16:04 +0700

    episode 9

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="d-flex align-items" >
-        <h1 class="mr-auto">BirdBoard</h1>
+    <div class="flex items-center mb-3">
         <a href="{{ url('/projects/create') }}">Create a New Projects</a>
     </div>
 
-    <ul>
+    <div class="flex">
         @forelse ($projects as $project) 
-            <li>
-                <a href="{{ url('projects/' . $project->id) }}">{{ $project->title }}</a>
-            </li>
+            <div class="bg-white mr-4 p-5 rounded shadow w-1/3" style="height: 200px">
+                <h3 class="text-xl font-normal py-4">{{ $project->title }}</h3>
+                <div class="text-gray-500">{{ Str::limit($project->description,100,' ...') }}</div>
+            </div>
         @empty
-            <li>No Project Yet</li>
+            <div>No Project Yet</div>
         @endforelse
-    </ul>
+    </div>
 @endsection
