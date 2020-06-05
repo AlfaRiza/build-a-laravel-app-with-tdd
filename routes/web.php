47669c50381ab0f7242784c8ca6ee83983c28f0b
AlfaRiza <malfariza45@gmail.com>
2020-06-05 19:42:05 +0700

    episode 33

--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,8 @@ Route::group(['middleware' => 'auth'], function(){
     // Route::post('/projects', 'ProjectsController@store');
     // Route::delete('/projects/{project', 'ProjectsController@destroy');
     Route::resource('projects', 'ProjectsController');
+
+    Route::post('/projects/{project}/invitations', 'ProjectInvitationsController@store');
     
     Route::post('/projects/{project}/tasks', 'ProjectTasksController@store');
     Route::patch('/projects/{project}/tasks/{task}', 'ProjectTasksController@update');
